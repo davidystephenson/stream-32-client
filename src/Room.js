0d29ba773b8588ca520d766d3d7a21e666abb1e3
David Y. Stephenson <david@davidystephenson.com>
2019-11-19 17:36:00 +0100

    Display user list

--- src/Room.js
+++ src/Room.js
@@ -1,17 +1,67 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import superagent from 'superagent'
 
 class Room extends React.Component {
+  onClick = async () => {
+    const { jwt, match } = this.props
+    const { name } = match.params
+
+    const url = `http://localhost:4000/join/${name}`
+
+    const response = await superagent
+      .put(url)
+      .set({
+        authorization: `Bearer ${jwt}`
+      })
+
+    console.log('response test:', response)
+  }
+
   render () {
     const { name } = this.props.match.params
-    console.log('name test:', name)
+
+    const { rooms } = this.props
+
+    // const { rooms, match } = this.prop
+    // const { name } = match.params
+
+    console.log('rooms test:', rooms)
+
+    const room = rooms
+      .find(room => room.name === name)
+
+    if (!room) {
+      return 'This room does not exist'
+    }
+
+    const { users } = room
+
+    const list = users.map(user =>
+      <p key={user.name}>
+        {user.name}
+      </p>
+    )
+
+    console.log('room test:', room)
 
     return <div>
       <h1>{name}</h1>
 
-      <button>Join</button>
+      <button onClick={this.onClick}>
+        Join
+      </button>
+
+      {list}
     </div>
   }
 }
 
-export default connect()(Room)
+function mapStateToProps (state) {
+  return {
+    jwt: state.jwt,
+    rooms: state
+  }
+}
+
+export default connect(mapStateToProps)(Room)
