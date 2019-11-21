cc7be6b1f0b930112cc593d52756512b3041e226
David Y. Stephenson <david@davidystephenson.com>
2019-11-21 12:10:39 +0100

    Give button

--- src/Room.js
+++ src/Room.js
@@ -4,8 +4,10 @@ import superagent from 'superagent'
 
 class Room extends React.Component {
   onClick = async () => {
-    const { jwt, match } = this.props
+    const { match } = this.props
     const { name } = match.params
+
+    const jwt = 'afakejwt'
 
     const url = `http://localhost:4000/join/${name}`
 
@@ -16,6 +18,24 @@ class Room extends React.Component {
       })
 
     console.log('response test:', response)
+  }
+
+  give = async (username) => {
+    // const jwt = 'afakejwt'
+
+    const { jwt } = this.props  
+
+    const url = `http://localhost:4000/points`
+
+    const response = await superagent
+      .put(url)
+      .send({ username })
+      .set({
+        authorization: `Bearer ${jwt}`
+      })
+
+    console
+      .log('response test:', response)
   }
 
   render () {
@@ -40,19 +60,38 @@ class Room extends React.Component {
     const list = users && users.length
       ? users.map(user =>
         <p key={user.name}>
-          {user.name} [{user.points}]
+          {user.name}
+          [{user.points}]
+          <button
+            onClick={
+              () => this.give(user.name)
+            }
+          >
+            Give
+          </button>
         </p>
       )
       : <p>This room has no users</p>
+
+    const joined = users
+      .some(user => user.name === 'david')
+
+    console.log('joined test:', joined)
+
+    const join = !joined && <button
+      onClick={this.onClick}
+    >
+      Join
+    </button>
+
+    console.log('join test:', join)
 
     console.log('list test:', list)
 
     return <div>
       <h1>{name}</h1>
 
-      <button onClick={this.onClick}>
-        Join
-      </button>
+      {join}
 
       {list}
     </div>
